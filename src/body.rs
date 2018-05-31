21efc0fe1f416f9c8e454b1342a34583d796d8a2
Ashton Hunger <ahunger@pdx.edu>
2018-05-31 15:16:34 -0700

    ship angled properly

--- src/body.rs
+++ src/body.rs
@@ -11,13 +11,13 @@ pub struct Body {
 }
 
 impl Body {
-    pub fn new(x: f32, y: f32, width: f32, height: f32, collidable: bool) -> Body {
+    pub fn new(x: f32, y: f32, width: f32, height: f32, rotation: f32, collidable: bool) -> Body {
         Body {
             pos: Vector2::new(x, y),
             size: Vector2::new(width, height),
             collidable,
             velocity: Vector2::new(0.0, 0.0),
-            rotation: 0.0
+            rotation
         }
     }
 }
--- src/player.rs
+++ src/player.rs
@@ -1,3 +1,4 @@
+use std::f32;
 use ggez::*;
 use nalgebra::Point2;
 use super::entity::*;
@@ -11,19 +12,29 @@ pub struct Player {
 impl Player {
     pub fn new() -> Player {
         Player {
-            body: Body::new(0.0, 0.0, 10.0, 10.0, true)
+            body: Body::new(300.0, 300.0, 10.0, 10.0, f32::consts::PI/2.0, true)
         }
     }
 }
 
 impl Entity for Player {
     fn update(&mut self) {
-
+        
     }
 
     fn draw(&self, asset_manager: &AssetManager, ctx: &mut Context, interpolation_value: f32) {
+        let image_dim = asset_manager.player.get_dimensions();
         let body = &self.body;
-        graphics::draw(ctx, &asset_manager.player, Point2::new(body.pos.x, body.pos.y), 0.0);
+        graphics::draw_ex(
+            ctx,
+            &asset_manager.player,
+            graphics::DrawParam {
+                dest: Point2::new(body.pos.x, body.pos.y),
+                rotation: body.rotation,
+                offset: Point2::new(0.5, 0.5),
+                .. Default::default()
+            }
+        ).unwrap();
     }
 
     fn get_body(&self) -> Option<Body> {
